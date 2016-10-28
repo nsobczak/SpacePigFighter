ec0cb88df75e2dcf1a31dcef17c02e670ab75394
nsobczak <nicolas.sobczak@isen.yncrea.fr>
2016-10-28 18:36:16 +0200

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * ===== Class Main =====
+ * 
+ * @author Nicolas Sobczak
+ * @version 1.01, 10/2016
+ */
+public class Main {
+
+	/**
+	 * main function
+	 * 
+	 * @param args
+	 * @return nothing
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
